349b28e52fe9909eee3c67ba892871c5ac1febb3
potter80511@gmail.com <potter80511@gmail.com>
2024-04-17 23:40:05 +0800

    Update home text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,9 +2,9 @@ import "./Home.css";
 
 function Home() {
   return <div className="home">
-    <h1>海碩科技</h1>
-    <p>本公司專精在氣動、電動工具的研發與製造其中氣動工具裡的錘打組機構，顛覆傳統的鑽石級「氣動衝擊扳手」在此問世了，其製作方式不同於傳統鍛造方式，透過真空上吸法，精密製造出的錘打機構壽命和強度都更勝一籌！</p>
-    <p>為了突破傳統的受限，本公司生產出的「氣動衝擊扳手」具有高扭力、高壽命、精準扭矩設值、防止移位氣缸組，耐摔、兩段式板機控制，單手操作正逆轉閥，使用得心應手，四段式精準氣壓控制，完美的氣壓流道設計，成品經廠內精進壽命試驗機連續不斷測試，壽命超過市面上的產品，零件加工尺寸精準，並經嚴謹的組裝及出貨前的測試，以極具競爭力的價格提供使用者拆卸和鎖固螺絲，是提升工作效率的最佳選擇且將台灣氣動工具水準提升至另一個新境界。</p>
+    <h1>Beyond the Scene</h1>
+    <p>Our company specializes in the research and development and manufacture of pneumatic and electric tools. Among them, the hammering mechanism in pneumatic tools has subverted the traditional diamond-level "pneumatic impact wrench". The production method is different from the traditional forging method. The suction method, the precision-made hammering mechanism has better life and strength!</p>
+    <p>In order to break through the traditional limitations, the "Pneumatic Impact Wrench" produced by our company has high torque, long life, precise torque setting, anti-shift cylinder group, drop resistance, two-stage trigger control, and one-hand operation for forward and reverse. The valve is easy to use, four-stage precise air pressure control, and perfect air pressure flow channel design. Pre-shipment testing, providing users with dismantling and locking screws at a very competitive price, is the best choice to improve work efficiency and raise the level of Taiwan's air tools to a new level.</p>
   </div>
 }
 
