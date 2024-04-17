30a177bf861611dc0950ade360cb524f420c5764
potter80511@gmail.com <potter80511@gmail.com>
2024-04-17 23:38:13 +0800

    Update account-info styles

--- src/pages/dashboard/AccountInfo.js
+++ src/pages/dashboard/AccountInfo.js
@@ -3,10 +3,19 @@ import { useContext } from 'react';
 
 function AccountInfo() {
   const { loginData } = useContext(LoginDataContext)
+  const accountData = [
+    {label: 'Account', value: loginData.account},
+    {label: 'Password', value: loginData.password}
+  ]
+  console.log(accountData)
 
   return <div className="account-info">
-    <div>account: {loginData.account}</div>
-    <div>password: {loginData.password}</div>
+    {accountData.map(({label, value}) => {
+      return <div key={label} className="field">
+        <div className='label'>{label}:</div>
+        <div className='value'>{value}</div>
+      </div>
+    })}
   </div>
 }
 
